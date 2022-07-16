cae0ffb30d5112c2178a30fdaa503f45ab8a7e01
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-15 21:34:56 -0400

    Update for MIO API changes

--- dv/uvme_ral_st/src/seq/uvme_ral_st_rand_access_seq.sv
+++ dv/uvme_ral_st/src/seq/uvme_ral_st_rand_access_seq.sv
@@ -11,66 +11,60 @@
  * TODO Describe uvme_ral_st_rand_access_seq_c
  */
 class uvme_ral_st_rand_access_seq_c extends uvme_ral_st_base_seq_c;
-   
+
    // Fields
-   rand int unsigned  num_accesses; ///< 
-   
-   
+   rand int unsigned  num_accesses; ///<
+
+
    `uvm_object_utils_begin(uvme_ral_st_rand_access_seq_c)
       `uvm_field_int(num_accesses, UVM_DEFAULT + UVM_DEC)
    `uvm_object_utils_end
-   
-   
+
+
    constraint limits_cons {
       num_accesses > 0;
       num_accesses <= 100;
    }
-   
-   
+
+
    /**
     * Default constructor.
     */
    extern function new(string name="uvme_ral_st_reg_vseq");
-   
+
    /**
     * TODO Describe uvme_ral_st_rand_access_seq_c::body()
     */
    extern virtual task body();
-   
+
 endclass : uvme_ral_st_rand_access_seq_c
 
 
 function uvme_ral_st_rand_access_seq_c::new(string name="uvme_ral_st_reg_vseq");
-   
+
    super.new(name);
-   
+
 endfunction : new
 
 
 task uvme_ral_st_rand_access_seq_c::body();
-   
+
    uvm_reg         my_regs[$] ;
    uvm_reg         current_reg;
    uvm_status_e    status     ;
    uvm_reg_data_t  value      ;
-   
+
    cfg.reg_block.get_registers(my_regs);
-   my_regs.shuffle();
-   
    for (int unsigned ii=0; ii<num_accesses; ii++) begin
-      current_reg = my_regs.pop_front();
+      current_reg = my_regs[$urandom_range(0,(my_regs.size()-1))];
       `uvm_info("TRAFFIC_VSEQ", $sformatf("Performing access #%0d of %0d:\n%s", ii+1, num_accesses, current_reg.sprint()), UVM_LOW)
       randcase
-         2: begin
-            current_reg.read(status, value);
-         end
-         1: begin
-            current_reg.write(status, $urandom());
-         end
+         2: current_reg.read (status, value);
+         1: current_reg.write(status, $urandom());
       endcase
       #100ns;
    end
-   
+
 endtask : body
 
 
--- dv/uvmt_ral_st/bin/main_release_regression.sh
+++ dv/uvmt_ral_st/bin/main_release_regression.sh
@@ -5,10 +5,10 @@
 ########################################################################################################################
 
 
-# Launched from uvml project sim dir
-python ./setup_project.py
-source ./setup_terminal.sh
-../tools/.imports/mio/src/__main__.py cpel uvmt_ral_st
-../tools/.imports/mio/src/__main__.py sim uvmt_ral_st -t front_door_access -s 1 -c
-../tools/.imports/mio/src/__main__.py results uvmt_ral_st results
-../tools/.imports/mio/src/__main__.py cov uvmt_ral_st
+# Launched from uvml_ral project sim dir
+shopt -s expand_aliases
+source ~/.bashrc
+mio cpel    uvmt_ral_st
+mio sim     uvmt_ral_st -t front_door_access -s 1 -c
+mio results uvmt_ral_st results
+mio cov     uvmt_ral_st
