0e4f6000a51b5f64bf3041a84e932376ac9b588b
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:31:40 -0400

    updating regressions to new CLI

--- dv/uvmt_ral_st/bin/sanity_regression.sh
+++ dv/uvmt_ral_st/bin/sanity_regression.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 ########################################################################################################################
-## Copyright 2021 Datum Technology Corporation
+## Copyright 2021-2022 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
 
 
 # Launched from uvml_ral project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_ral_st
-mio sim     uvmt_ral_st -t front_door_access -s 1 -c
+mio cpel    uvmt_ral_st -CE
+mio sim     uvmt_ral_st -S -t front_door_access -s 1 -c
 mio results uvmt_ral_st results
 mio cov     uvmt_ral_st
